755dda0d81b833d79262ec58a52124ac6f490d31
Rebeca Gonzalez <bequipedia@gmail.com>
2023-05-18 12:39:32 +0200

    add failing test for print state with second cell marked when first player marked two

--- src/test/java/BoardShould.java
+++ src/test/java/BoardShould.java
@@ -4,22 +4,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BoardShould {
 
-//    @Test
-//    void print_default_state() {
-//        Board board = new Board();
-//
-//        String boardState = board.printCurrentState();
-//
-//        String expectedState = """
-//                +---+---+---+
-//                | 1 | 2 | 3 |
-//                +---+---+---+
-//                | 4 | 5 | 6 |
-//                +---+---+---+
-//                | 7 | 8 | 9 |
-//                +---+---+---+""";
-//        assertEquals(expectedState, boardState);
-//    }
+    @Test
+    void print_default_state() {
+        Board board = new Board();
+
+        String boardState = board.printCurrentState();
+
+        String expectedState = """
+                +---+---+---+
+                | 1 | 2 | 3 |
+                +---+---+---+
+                | 4 | 5 | 6 |
+                +---+---+---+
+                | 7 | 8 | 9 |
+                +---+---+---+""";
+        assertEquals(expectedState, boardState);
+    }
 
     @Test
     void print_state_with_first_cell_marked_when_firs_player_marked_one() {
@@ -40,14 +40,15 @@ public class BoardShould {
     }
 
     @Test
-    void print_state_when_first_cell_is_marked() {
+    void print_state_with_second_cell_marked_when_firs_player_marked_two() {
         Board board = new Board();
 
+        board.markCell(2);
         String boardState = board.printCurrentState();
 
         String expectedState = """
                 +---+---+---+
-                | x | 2 | 3 |
+                | 1 | X | 3 |
                 +---+---+---+
                 | 4 | 5 | 6 |
                 +---+---+---+
