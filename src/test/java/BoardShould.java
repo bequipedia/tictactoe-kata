e917a355d7caa050a5f5f720c7972da810bed99f
K Leonardo Ruiz Flórez <kleonardoruizflorez@gmail.com>
2023-05-18 12:12:03 +0200

    Add failing test for print state when first cell is marked

--- src/test/java/BoardShould.java
+++ src/test/java/BoardShould.java
@@ -20,4 +20,21 @@ public class BoardShould {
                 +---+---+---+""";
         assertEquals(expectedState, boardState);
     }
+
+    @Test
+    void print_state_when_first_cell_is_marked() {
+        Board board = new Board();
+
+        String boardState = board.printCurrentState();
+
+        String expectedState = """
+                +---+---+---+
+                | x | 2 | 3 |
+                +---+---+---+
+                | 4 | 5 | 6 |
+                +---+---+---+
+                | 7 | 8 | 9 |
+                +---+---+---+""";
+        assertEquals(expectedState, boardState);
+    }
 }
