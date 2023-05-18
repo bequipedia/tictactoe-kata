295c3ac2871dd47934405c12bdb07d0f7f24d2dd
Rebeca Gonzalez <bequipedia@gmail.com>
2023-05-18 16:38:47 +0200

    refactor to remove duplicated code. Remove if statements and add a for loop to iterate between the different three cases

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,54 +1,35 @@
+import java.util.Arrays;
+
 public class Board {
+    private String[] firstRowMarks = new String[3];
 
     private int markedCell;
 
     public Board() {
     }
-
     public Board(int markedCell) {
         this.markedCell = markedCell;
     }
 
     public String printCurrentState() {
-        if(markedCell == 1) {
-            return """
-                +---+---+---+
-                | X | 2 | 3 |
-                +---+---+---+
+        if(markedCell != 0) {
+            firstRowMarks[markedCell -1] = "X";
+        }
+        String[] firstRowCells = firstRowMarks;
+        for(int cell = 0; cell < firstRowMarks.length; cell++) {
+            boolean isMarked = firstRowMarks[cell] != null;
+            if(!isMarked) {
+                firstRowCells[cell] = String.valueOf(cell + 1);
+            }
+        }
+        return "+---+---+---+\n| "+
+                    String.join(" | ", firstRowCells) +
+                """
+                 |\n+---+---+---+
                 | 4 | 5 | 6 |
                 +---+---+---+
                 | 7 | 8 | 9 |
                 +---+---+---+""";
         }
 
-        if(markedCell == 2) {
-            return """
-                +---+---+---+
-                | 1 | X | 3 |
-                +---+---+---+
-                | 4 | 5 | 6 |
-                +---+---+---+
-                | 7 | 8 | 9 |
-                +---+---+---+""";
-        }
-        if(markedCell == 3) {
-            return """
-                +---+---+---+
-                | 1 | 2 | X |
-                +---+---+---+
-                | 4 | 5 | 6 |
-                +---+---+---+
-                | 7 | 8 | 9 |
-                +---+---+---+""";
-        }
-
-    return """
-                +---+---+---+
-                | 1 | 2 | 3 |
-                +---+---+---+
-                | 4 | 5 | 6 |
-                +---+---+---+
-                | 7 | 8 | 9 |
-                +---+---+---+""";
-    }
 }
--- src/test/java/BoardShould.java
+++ src/test/java/BoardShould.java
@@ -71,4 +71,6 @@ public class BoardShould {
                 +---+---+---+""";
         assertEquals(expectedState, boardState);
     }
+
+
 }
